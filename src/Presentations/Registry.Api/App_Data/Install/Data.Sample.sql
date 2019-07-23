6164913119cff65598a07722cb7b553fdd4cb368
Akhmed Shamsudinov <shamsudinov.akhmed@gmail.com>
2019-07-24 02:05:33 +0400

    Add sample data

--- src/Presentations/Registry.Api/App_Data/Install/Data.Sample.sql
+++ src/Presentations/Registry.Api/App_Data/Install/Data.Sample.sql
@@ -1,1 +1,15 @@
-﻿
+﻿SET IDENTITY_INSERT dbo.AreaType ON; 
+INSERT INTO dbo.AreaType ([Id], [Name]) 
+	VALUES	(1, N'Земельный участок'),
+			(2, N'Единое землепользование'),
+			(3, N'Часть земельного участка');
+SET IDENTITY_INSERT dbo.Company OFF; 
+
+SET IDENTITY_INSERT dbo.AreaOwnershipType ON; 
+INSERT INTO dbo.AreaOwnershipType ([Id], [Name]) 
+	VALUES	(1, N'Собственность'),
+			(2, N'Пожизненное наследуемое владение'),
+			(3, N'Постоянное (бессрочное)пользование'),
+			(4, N'Аренда'),
+			(5, N'Оперативное управление');
+SET IDENTITY_INSERT dbo.Company OFF; 
