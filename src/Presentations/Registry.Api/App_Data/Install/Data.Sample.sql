5172dfaa5bf8378254b22619ad3b478121614593
Akhmed Shamsudinov <shamsudinov.akhmed@gmail.com>
2019-07-24 02:07:03 +0400

    Update Data.Sample.sql

--- src/Presentations/Registry.Api/App_Data/Install/Data.Sample.sql
+++ src/Presentations/Registry.Api/App_Data/Install/Data.Sample.sql
@@ -3,7 +3,7 @@ INSERT INTO dbo.AreaType ([Id], [Name])
 	VALUES	(1, N'Земельный участок'),
 			(2, N'Единое землепользование'),
 			(3, N'Часть земельного участка');
-SET IDENTITY_INSERT dbo.Company OFF; 
+SET IDENTITY_INSERT dbo.AreaType OFF; 
 
 SET IDENTITY_INSERT dbo.AreaOwnershipType ON; 
 INSERT INTO dbo.AreaOwnershipType ([Id], [Name]) 
@@ -12,4 +12,4 @@ INSERT INTO dbo.AreaOwnershipType ([Id], [Name])
 			(3, N'Постоянное (бессрочное)пользование'),
 			(4, N'Аренда'),
 			(5, N'Оперативное управление');
-SET IDENTITY_INSERT dbo.Company OFF; 
+SET IDENTITY_INSERT dbo.AreaOwnershipType OFF; 
