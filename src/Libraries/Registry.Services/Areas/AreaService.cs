d8b1c049557925ee7317c16ab8ebde9fa99072fd
Akhmed Shamsudinov <shamsudinov.akhmed@gmail.com>
2019-07-24 02:24:04 +0400

    Implementing an area service

--- src/Libraries/Registry.Services/Areas/AreaService.cs
+++ src/Libraries/Registry.Services/Areas/AreaService.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Registry.Core.Data;
+using Registry.Core.Domain.Areas;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Registry.Services.Areas
@@ -7,7 +10,105 @@ namespace Registry.Services.Areas
     /// <summary>
     /// Area service
     /// </summary>
-    public partial class AreaService
+    public partial class AreaService : IAreaService
     {
+        #region Fields
+        
+        private readonly IRepository<Area> _areaRepository;
+
+        #endregion
+
+        #region Ctor
+
+        public AreaService(IRepository<Area> areaRepository)
+        {
+            _areaRepository = areaRepository;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Gets area by area identifier
+        /// </summary>
+        /// <param name="areaId">Area identifier</param>
+        /// <returns>Area</returns>
+        public Area GetAreaById(int areaId)
+        {
+            if (areaId == 0)
+                return null;
+
+            return _areaRepository.GetById(areaId);
+        }
+
+        /// <summary>
+        /// Gets all areas
+        /// </summary>
+        /// <returns>Areas</returns>
+        public IList<Area> GetAreasAll()
+        {
+            var query = _areaRepository.Table;
+            return query.ToList();
+        }
+
+        /// <summary>
+        /// Gets areas by areas identifiers
+        /// </summary>
+        /// <param name="blogPostIds">Areas identifiers</param>
+        /// <returns>Areas</returns>
+        public IList<Area> GetAreasByIds(int[] areaIds)
+        {
+            var query = _areaRepository.Table;
+            return query.Where(area => areaIds.Contains(area.Id)).ToList();
+        }
+
+        /// <summary>
+        /// Gets an areas by poligon
+        /// </summary>
+        /// <param name="poligon">Poligon</param>
+        /// <returns>Areas</returns>
+        public IList<Area> GetAreasByPoligon(object poligon)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Marks area as deleted 
+        /// </summary>
+        /// <param name="area">Area</param>
+        public void DeleteArea(Area area)
+        {
+            if (area == null)
+                throw new ArgumentNullException(nameof(area));
+
+            _areaRepository.Delete(area);
+        }
+
+        /// <summary>
+        /// Inserts an area
+        /// </summary>
+        /// <param name="area">Area</param>
+        public void InsertArea(Area area)
+        {
+            if (area == null)
+                throw new ArgumentNullException(nameof(area));
+
+            _areaRepository.Insert(area);
+        }
+
+        /// <summary>
+        /// Updates the area
+        /// </summary>
+        /// <param name="area">Area</param>
+        public void UpdateArea(Area area)
+        {
+            if (area == null)
+                throw new ArgumentNullException(nameof(area));
+
+            _areaRepository.Update(area);
+
+        }
+        #endregion
     }
 }
--- src/Libraries/Registry.Services/Areas/IAreaService.cs
+++ src/Libraries/Registry.Services/Areas/IAreaService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Registry.Core.Domain.Areas;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,5 +10,49 @@ namespace Registry.Services.Areas
     /// </summary>
     public partial interface IAreaService
     {
+        /// <summary>
+        /// Gets area by area identifier
+        /// </summary>
+        /// <param name="areaId">Area identifier</param>
+        /// <returns>Area</returns>
+        Area GetAreaById(int areaId);
+
+        /// <summary>
+        /// Gets all areas
+        /// </summary>
+        /// <returns>Areas</returns>
+        IList<Area> GetAreasAll();
+
+        /// <summary>
+        /// Gets areas by areas identifiers
+        /// </summary>
+        /// <param name="blogPostIds">Areas identifiers</param>
+        /// <returns>Areas</returns>
+        IList<Area> GetAreasByIds(int[] areaIds);
+
+        /// <summary>
+        /// Gets an areas by poligon
+        /// </summary>
+        /// <param name="poligon">Poligon</param>
+        /// <returns>Areas</returns>
+        IList<Area> GetAreasByPoligon(object poligon);
+
+        /// <summary>
+        /// Marks area as deleted 
+        /// </summary>
+        /// <param name="area">Area</param>
+        void DeleteArea(Area area);
+
+        /// <summary>
+        /// Inserts an area
+        /// </summary>
+        /// <param name="area">Area</param>
+        void InsertArea(Area area);
+
+        /// <summary>
+        /// Updates the area
+        /// </summary>
+        /// <param name="area">Area</param>
+        void UpdateArea(Area area);
     }
 }
